8c908c21152d02c675ea5fa07cdcab93511de2dc
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-31 23:29:52 +0600

    List items in map method

--- src/List.tsx
+++ src/List.tsx
@@ -1,7 +1,9 @@
 function List(){
 
   const fruits =["apple", "orange", "banana", "coconut", "pineapple"];
+  
+  const listItems = fruits.map(fruit => <li>fruit</li>);
 
-  return(fruits);
+  return(<ul>{listItems}</ul>);
 }
 export default List;
