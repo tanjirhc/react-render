ebf31fd253b9d50c4e673ee7c01ff4ed13bac24e
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-31 23:30:33 +0600

    fruit in curly braces

--- src/List.tsx
+++ src/List.tsx
@@ -2,7 +2,7 @@ function List(){
 
   const fruits =["apple", "orange", "banana", "coconut", "pineapple"];
   
-  const listItems = fruits.map(fruit => <li>fruit</li>);
+  const listItems = fruits.map(fruit => <li>{fruit}</li>);
 
   return(<ul>{listItems}</ul>);
 }
