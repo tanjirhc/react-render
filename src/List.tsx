d516b30e1c29afafdbc4e8f1c657e0c6c6a03814
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-01 20:12:14 +0600

    unique id

--- src/List.tsx
+++ src/List.tsx
@@ -1,14 +1,14 @@
 function List(){
 
-  const fruits =[{name:"apple", calories: 95}, 
-                  {name: "orange", calories: 45}, 
-                  {name: "banana", calories: 37}, 
-                  {name: "coconut", calories: 150}, 
-                  {name: "pineapple", calories: 55}];
+  const fruits =[{id: 1, name:"apple", calories: 95}, 
+                  {id: 2, name: "orange", calories: 45}, 
+                  {id: 3, name: "banana", calories: 37}, 
+                  {id: 4, name: "coconut", calories: 150}, 
+                  {id: 5, name: "pineapple", calories: 55}];
 
   fruits.sort();
   
-  const listItems = fruits.map(fruit => <li>{fruit.name}</li>);
+  const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}</li>);
 
   return(<ol>{listItems}</ol>);
 }
