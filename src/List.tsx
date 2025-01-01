13053d34880d5a6981dd8c617d6dcf55589d12be
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-01 20:17:11 +0600

    reverse order

--- src/List.tsx
+++ src/List.tsx
@@ -1,19 +1,22 @@
-function List(){
+function List() {
+  const fruits = [
+    { id: 1, name: "apple", calories: 95 },
+    { id: 2, name: "orange", calories: 45 },
+    { id: 3, name: "banana", calories: 37 },
+    { id: 4, name: "coconut", calories: 150 },
+    { id: 5, name: "pineapple", calories: 55 },
+  ];
 
-  const fruits =[{id: 1, name:"apple", calories: 95}, 
-                  {id: 2, name: "orange", calories: 45}, 
-                  {id: 3, name: "banana", calories: 37}, 
-                  {id: 4, name: "coconut", calories: 150}, 
-                  {id: 5, name: "pineapple", calories: 55}];
+  //                  fruits.sort((a, b) => a.name.localeCompare(b.name)); //alphabetic
+  fruits.sort((a, b) => b.name.localeCompare(a.name)); //reverse order
 
-                  fruits.sort((a, b) => a.name.localeCompare(b.name));
+  const listItems = fruits.map((fruit) => (
+    <li key={fruit.id}>
+      {fruit.name}: &nbsp;
+      {fruit.calories}
+    </li>
+  ));
 
-  fruits.sort();
-  
-  const listItems = fruits.map(fruit => <li key={fruit.id}>
-                                          {fruit.name}: &nbsp;
-                                          {fruit.calories}</li>);
-
-  return(<ol>{listItems}</ol>);
+  return <ol>{listItems}</ol>;
 }
 export default List;
