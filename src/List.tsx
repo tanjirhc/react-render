ea4b366809d6d4014189e16c65629a262097d8a3
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-01 20:08:53 +0600

    showing name property

--- src/List.tsx
+++ src/List.tsx
@@ -1,10 +1,14 @@
 function List(){
 
-  const fruits =["apple", "orange", "banana", "coconut", "pineapple"];
+  const fruits =[{name:"apple", calories: 95}, 
+                  {name: "orange", calories: 45}, 
+                  {name: "banana", calories: 37}, 
+                  {name: "coconut", calories: 150}, 
+                  {name: "pineapple", calories: 55}];
 
   fruits.sort();
   
-  const listItems = fruits.map(fruit => <li>{fruit}</li>);
+  const listItems = fruits.map(fruit => <li>{fruit.name}</li>);
 
   return(<ol>{listItems}</ol>);
 }
