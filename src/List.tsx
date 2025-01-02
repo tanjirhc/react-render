59d3bd507c3c2d375713421ce2adb763b06368db
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-02 21:08:22 +0600

    filter method

--- src/List.tsx
+++ src/List.tsx
@@ -10,9 +10,11 @@ function List() {
   //                  fruits.sort((a, b) => a.name.localeCompare(b.name)); //alphabetic
   //fruits.sort((a, b) => b.name.localeCompare(a.name)); //reverse order
   //fruits.sort((a, b) => a.calories - b.calories); // asceding
-  fruits.sort((a, b) => b.calories - a.calories); // desceding
+  // fruits.sort((a, b) => b.calories - a.calories); // desceding
 
-  const listItems = fruits.map((fruit) => (
+  const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+
+  const listItems = lowCalFruits.map((fruit) => (
     <li key={fruit.id}>
       {fruit.name}: &nbsp;
       {fruit.calories}
