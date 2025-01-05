834cbc6f915b7ce0d9e2c1258cf4072223b76a2a
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-05 23:44:37 +0600

    reusable list component

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,26 @@ import List from "./List.tsx";
 
 function App() {
   const fruits = [
-                  { id: 1, name: "apple", calories: 95 },
-                  { id: 2, name: "orange", calories: 45 },
-                  { id: 3, name: "banana", calories: 37 },
-                  { id: 4, name: "coconut", calories: 150 },
-                  { id: 5, name: "pineapple", calories: 55 },
-                 ];
+    { id: 1, name: "apple", calories: 95 },
+    { id: 2, name: "orange", calories: 45 },
+    { id: 3, name: "banana", calories: 37 },
+    { id: 4, name: "coconut", calories: 150 },
+    { id: 5, name: "pineapple", calories: 55 },
+  ];
   const vegetables = [
-                      { id: 6, name: "potatoes", calories: 110 },
-                      { id: 7, name: "celery", calories: 15 },
-                      { id: 8, name: "carrots", calories: 25 },
-                      { id: 9, name: "corn", calories: 63 },
-                      { id: 10, name: "broccoli", calories: 50 },
-                    ];
+    { id: 6, name: "potatoes", calories: 110 },
+    { id: 7, name: "celery", calories: 15 },
+    { id: 8, name: "carrots", calories: 25 },
+    { id: 9, name: "corn", calories: 63 },
+    { id: 10, name: "broccoli", calories: 50 },
+  ];
 
-  return( <>
-            <List items={fruits} category="Fruits"/>
-            <List items={vegetables} category="Vegetables"/>
-          </>);
+  return (
+    <>
+      <List items={fruits} category="Fruits" />
+      <List items={vegetables} category="Vegetables" />
+    </>
+  );
 }
 
 export default App;
