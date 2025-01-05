51b0e2ec1227fa84bdb51bc681fd8f6f4d37b921
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-05 23:43:53 +0600

    another category

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,18 @@ function App() {
                   { id: 4, name: "coconut", calories: 150 },
                   { id: 5, name: "pineapple", calories: 55 },
                  ];
+  const vegetables = [
+                      { id: 6, name: "potatoes", calories: 110 },
+                      { id: 7, name: "celery", calories: 15 },
+                      { id: 8, name: "carrots", calories: 25 },
+                      { id: 9, name: "corn", calories: 63 },
+                      { id: 10, name: "broccoli", calories: 50 },
+                    ];
 
-  return <List items={fruits} category="Fruits"/>;
+  return( <>
+            <List items={fruits} category="Fruits"/>
+            <List items={vegetables} category="Vegetables"/>
+          </>);
 }
 
 export default App;
