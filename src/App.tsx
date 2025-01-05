a03a8af754b239cc3c982b465e7d1bc23455ea03
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-05 23:34:23 +0600

    using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import List from "./List.tsx"
-
+import List from "./List.tsx";
 
 function App() {
+  const fruits = [
+                  { id: 1, name: "apple", calories: 95 },
+                  { id: 2, name: "orange", calories: 45 },
+                  { id: 3, name: "banana", calories: 37 },
+                  { id: 4, name: "coconut", calories: 150 },
+                  { id: 5, name: "pineapple", calories: 55 },
+                 ];
 
-  return (
-    <>
-      <List />
-    </>
-  )
+  return <List items={fruits} category="Fruits"/>;
 }
 
-export default App
+export default App;
--- src/List.tsx
+++ src/List.tsx
@@ -1,24 +1,9 @@
-function List() {
-  const fruits = [
-    { id: 1, name: "apple", calories: 95 },
-    { id: 2, name: "orange", calories: 45 },
-    { id: 3, name: "banana", calories: 37 },
-    { id: 4, name: "coconut", calories: 150 },
-    { id: 5, name: "pineapple", calories: 55 },
-  ];
-
-  //                  fruits.sort((a, b) => a.name.localeCompare(b.name)); //alphabetic
-  //fruits.sort((a, b) => b.name.localeCompare(a.name)); //reverse order
-  //fruits.sort((a, b) => a.calories - b.calories); // asceding
-  // fruits.sort((a, b) => b.calories - a.calories); // desceding
-
-  // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
-  const highCalFruits = fruits.filter((fruit) => fruit.calories > 100);
-
-  const listItems = highCalFruits.map((highCalFruit) => (
-    <li key={highCalFruit.id}>
-      {highCalFruit.name}: &nbsp;
-      {highCalFruit.calories}
+function List(props) {
+  const itemList = props.items;
+  const listItems = itemList.map((item) => (
+    <li key={item.id}>
+      {item.name}: &nbsp;
+      <b>{item.calories}</b>{" "}
     </li>
   ));
 
