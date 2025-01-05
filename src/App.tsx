021ffbfa76f58c88c20c40d27b55404c1724e05b
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-06 00:02:15 +0600

    conditional shortcut = &&

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@ function App() {
 
   return (
     <>
-      {fruits.length > 0 ? <List items={fruits} category="Fruits" /> : null}
-      {vegetables.length > 0 ? <List items={vegetables} category="Vegetables" /> : null}
+      {fruits.length > 0 && <List items={fruits} category="Fruits" />}
+      {vegetables.length > 0 && <List items={vegetables} category="Vegetables" />}
     </>
   );
 }
